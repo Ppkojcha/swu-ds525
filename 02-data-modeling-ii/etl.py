b374104fab0ebf07706486cabb602826e86a7faf
Ppkojcha <111840507+Ppkojcha@users.noreply.github.com>
2022-10-02 19:08:52 +0700

    Update etl.py

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -1,49 +1,44 @@
+from cassandra.cluster import Cluster
 import glob
 import json
 import os
 from typing import List
 
-from cassandra.cluster import Cluster
+import psycopg2
 
 
-table_drop = "DROP TABLE events"
+#Drop table 
 
-table_create = """
-    CREATE TABLE IF NOT EXISTS events
-    (
-        id text,
-        type text,
-        public boolean,
-        PRIMARY KEY (
-            id,
-            type
-        )
+table_drop_actor = "DROP TABLE IF EXISTS Actor"
+table_drop_event = "DROP TABLE IF EXISTS Event"
+
+#สร้างตาราง
+
+table_create_actor = """
+    CREATE TABLE IF NOT EXISTS Actor (
+        id int,
+        login varchar,
+        display_login varchar,
+        PRIMARY KEY ((id),login)
     )
 """
 
+table_create_event = """
+    CREATE TABLE IF NOT EXISTS Event (
+        id bigint,
+        type varchar ,
+        create_at timestamp,
+        PRIMARY KEY ((id),create_at)
+        )
+"""
 create_table_queries = [
-    table_create,
+    table_create_actor,table_create_event
 ]
 drop_table_queries = [
-    table_drop,
+    table_drop_event,table_create_actor
 ]
 
-def drop_tables(session):
-    for query in drop_table_queries:
-        try:
-            rows = session.execute(query)
-        except Exception as e:
-            print(e)
-
-
-def create_tables(session):
-    for query in create_table_queries:
-        try:
-            session.execute(query)
-        except Exception as e:
-            print(e)
-
-
+#Fuction ดึงข้อมูลจาก .json 
 def get_files(filepath: str) -> List[str]:
     """
     Description: This function is responsible for listing the files in a directory
@@ -61,6 +56,24 @@ def get_files(filepath: str) -> List[str]:
     return all_files
 
 
+
+
+def drop_tables(session):
+    for query in drop_table_queries:
+        try:
+            rows = session.execute(query)
+        except Exception as e:
+            print(e)
+
+
+def create_tables(session):
+    for query in create_table_queries:
+        try:
+            session.execute(query)
+        except Exception as e:
+            print(e)
+
+
 def process(session, filepath):
     # Get list of files from filepath
     all_files = get_files(filepath)
@@ -69,17 +82,38 @@ def process(session, filepath):
         with open(datafile, "r") as f:
             data = json.loads(f.read())
             for each in data:
-                # Print some sample data
-                print(each["id"], each["type"], each["actor"]["login"])
+               
+                 # Insert data into Actor table 
+                
+                try:
+                    query_actor = "INSERT INTO Actor (id,login,display_login) \
+                        VALUES (%s, '%s', '%s')" \
+                        % (each["actor"]["id"], each["actor"]["login"], each["actor"]["display_login"])
+                except:
+                    query_actor = "INSERT INTO Actor (id,login,display_login) \
+                        VALUES (%s, '%s', '%s')" \
+                        % (each["actor"]["id"], each["actor"]["login"], each["actor"]["display_login"])
+              
+                    session.execute(query_actor)    
 
-                # Insert data into tables here
+                try:
+                    if each["payload"]["push_id"] != None :
+                        query_event = "INSERT INTO Event (id,type,create_at) \
+                            VALUES (%s, '%s', '%s')" \
+                            % (each["id"], each["type"], each["created_at"])
+                        session.execute(query_event)
+                        
+                    else :
+                        query_event = "INSERT INTO Event (id,type,create_at) \
+                            VALUES (%s, '%s', '%s')" \
+                            % (each["id"], each["type"], each["created_at"])
+                        session.execute(query_event)
+                        
+                except:
+                    pass
 
 
-def insert_sample_data(session):
-    query = f"""
-    INSERT INTO events (id, type, public) VALUES ('23487929637', 'IssueCommentEvent', true)
-    """
-    session.execute(query)
+
 
 
 def main():
@@ -105,16 +139,15 @@ def main():
 
     drop_tables(session)
     create_tables(session)
-
-    # process(session, filepath="../data")
-    insert_sample_data(session)
+    process(session, filepath="../data")
+    #insert_sample_data(session)
 
     # Select data in Cassandra and print them to stdout
-    query = """
-    SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
-    """
+    query1 = """
+    SELECT id,type,create_at from Event --WHERE public='True' ORDER BY created_at DESC
+     """
     try:
-        rows = session.execute(query)
+        rows = session.execute(query1)
     except Exception as e:
         print(e)
 
